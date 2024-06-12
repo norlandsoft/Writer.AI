ab6792f9cf0738d635c8854640a64845b540ee4b
norlandsoft <11669556@qq.com>
2024-06-13 00:18:44 +0800

    framework

---
+++ air-writer/src/main/java/com/norlandsoft/air/writer/AirWriter.java
@@ -0,0 +1,16 @@
+package com.norlandsoft.air.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ChaiMingXu, on 2024/6/13
+ */
+@SpringBootApplication
+public class AirWriter {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AirWriter.class, args);
+  }
+
+}
---
+++ frontend/Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="eric"
+
+ENTRYPOINT ["top", "-b"]
